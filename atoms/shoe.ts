bc4056e00e56c7864b14ce260e6fce01629daea0
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-04-16 10:04:54 +0900

    パーツのテクスチャ更新用atomを作成した

--- atoms/shoe.ts
+++ atoms/shoe.ts
@@ -1,6 +1,8 @@
 import { atom } from "jotai";
+type ShoePartTexture = "none"|"leather";
 type ShoePartMaterial = {
   color: string;
+  texture:ShoePartTexture;
 };
 export type ShoePart =
   | "laces"
@@ -13,17 +15,22 @@ export type ShoePart =
   | "patch";
 type ShoeConfig = Record<ShoePart,ShoePartMaterial>;
 export const shoeConfigAtom = atom<ShoeConfig>({
-  laces: { color: "white" },
-  mesh: { color: "white" },
-  caps: { color: "white" },
-  inner: { color: "white" },
-  sole: { color: "white" },
-  stripes: { color: "white" },
-  band: { color: "white" },
-  patch: { color: "white" },
+  laces: { color: "white" ,texture:"none"},
+  mesh: { color: "white" ,texture:"none"},
+  caps: { color: "white" ,texture:"none"},
+  inner: { color: "white" ,texture:"none"},
+  sole: { color: "white" ,texture:"none"},
+  stripes: { color: "white" ,texture:"none"},
+  band: { color: "white" ,texture:"none"},
+  patch: { color: "white" ,texture:"none"},
 });
 export const setShoePartColorAtom = atom(null,(get,set,update:{part:ShoePart,color:string}) => {
   const {part,color} = update;
   const shoeConfig= get(shoeConfigAtom);
   set(shoeConfigAtom,{...shoeConfig,[part]:{...shoeConfig[part],color}});
+});
+export const setShoePartTextureAtom = atom(null,(get,set,update:{part:ShoePart,texture:ShoePartTexture}) => {
+  const {part,texture} = update;
+  const shoeConfig = get(shoeConfigAtom);
+  set(shoeConfigAtom,{...shoeConfig,[part]:{...shoeConfig[part],texture}})
 });
