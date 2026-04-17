828f41bd06144b42cfeeff7a1c429c6894079250
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-04-17 20:04:01 +0900

    スライド番号、スライドを進める/戻す関数atomを作成した
    UIの動きに呼応して3dモデルのアニメーションを行うためにはスライド関連のステートをグローバルに管理する必要があったため

---
+++ atoms/slide.ts
@@ -0,0 +1,19 @@
+import { atom } from "jotai";
+import { ShoePart } from "./shoe";
+export const SHOE_PARTS: ShoePart[] = [
+  "mesh",
+  "stripes",
+  "laces",
+  "caps",
+  "inner",
+  "sole",
+  "band",
+  "patch",
+];
+export const slideNumAtom = atom<number>(0);
+export const nextSlideAtom = atom(null,(_,set) => {
+  set(slideNumAtom,slideNum=>(slideNum+1)%SHOE_PARTS.length);
+});
+export const prevSlideAtom = atom(null,(_,set) => {
+  set(slideNumAtom,slideNum=>(slideNum===0)?(SHOE_PARTS.length-1):(slideNum-1));
+})
